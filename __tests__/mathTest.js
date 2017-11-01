438a676d7197323e0bdf5a3678051ec5cf0d0b33
anik3tra0 <anik3tra0@gmail.com>
2017-11-01 11:23:18 +0530

    Validation Function Test

--- __tests__/mathTest.js
+++ __tests__/mathTest.js
@@ -3,3 +3,13 @@ const subtract = require('../lib/math').subtract;
 const multiply = require('../lib/math').multiply;
 const divide = require('../lib/math').divide;
 const Validate = require('../lib/math').Validate;
+
+describe('ifNumber', () => {
+  it('should validate a dataset if it contains numbers', () => {
+    expect(Validate.ifNumber([1,2,3,4,5])).toBeTruthy();
+  });
+  
+  it('should not validate a dataset if it contains characters', () => {
+    expect(Validate.ifNumber([1,2,'a',4,'z'])).toBeFalsy();
+  });
+});
