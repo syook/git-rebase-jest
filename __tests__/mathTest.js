0d4dd11dc8d4bee9f0db510abe9261560b642be4
anik3tra0 <anik3tra0@gmail.com>
2017-11-01 11:22:32 +0530

    Imported Math Functions

---
+++ __tests__/mathTest.js
@@ -0,0 +1,5 @@
+const add = require('../lib/math').add;
+const subtract = require('../lib/math').subtract;
+const multiply = require('../lib/math').multiply;
+const divide = require('../lib/math').divide;
+const Validate = require('../lib/math').Validate;
